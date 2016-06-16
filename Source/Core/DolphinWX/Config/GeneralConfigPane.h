c890ce7a993d32a43c26273c61ca3859ba17480f
Pierre Bourdon <delroth@gmail.com>
2016-06-16 22:58:17 +0200

    Reformat all the things.

--- Source/Core/DolphinWX/Config/GeneralConfigPane.h
+++ Source/Core/DolphinWX/Config/GeneralConfigPane.h
@@ -14,36 +14,35 @@ class wxRadioBox;
 class GeneralConfigPane final : public wxPanel
 {
 public:
-	GeneralConfigPane(wxWindow* parent, wxWindowID id);
+  GeneralConfigPane(wxWindow* parent, wxWindowID id);
 
 private:
+  struct CPUCore
+  {
+    int CPUid;
+    wxString name;
+  };
+  std::vector<CPUCore> cpu_cores;
+  void InitializeGUI();
+  void LoadGUIValues();
+  void RefreshGUI();
 
-	struct CPUCore
-	{
-		int CPUid;
-		wxString name;
-	};
-	std::vector<CPUCore> cpu_cores;
-	void InitializeGUI();
-	void LoadGUIValues();
-	void RefreshGUI();
+  void OnDualCoreCheckBoxChanged(wxCommandEvent&);
+  void OnIdleSkipCheckBoxChanged(wxCommandEvent&);
+  void OnCheatCheckBoxChanged(wxCommandEvent&);
+  void OnForceNTSCJCheckBoxChanged(wxCommandEvent&);
+  void OnThrottlerChoiceChanged(wxCommandEvent&);
+  void OnCPUEngineRadioBoxChanged(wxCommandEvent&);
 
-	void OnDualCoreCheckBoxChanged(wxCommandEvent&);
-	void OnIdleSkipCheckBoxChanged(wxCommandEvent&);
-	void OnCheatCheckBoxChanged(wxCommandEvent&);
-	void OnForceNTSCJCheckBoxChanged(wxCommandEvent&);
-	void OnThrottlerChoiceChanged(wxCommandEvent&);
-	void OnCPUEngineRadioBoxChanged(wxCommandEvent&);
+  wxArrayString m_throttler_array_string;
+  wxArrayString m_cpu_engine_array_string;
 
-	wxArrayString m_throttler_array_string;
-	wxArrayString m_cpu_engine_array_string;
+  wxCheckBox* m_dual_core_checkbox;
+  wxCheckBox* m_idle_skip_checkbox;
+  wxCheckBox* m_cheats_checkbox;
+  wxCheckBox* m_force_ntscj_checkbox;
 
-	wxCheckBox* m_dual_core_checkbox;
-	wxCheckBox* m_idle_skip_checkbox;
-	wxCheckBox* m_cheats_checkbox;
-	wxCheckBox* m_force_ntscj_checkbox;
+  wxChoice* m_throttler_choice;
 
-	wxChoice* m_throttler_choice;
-
-	wxRadioBox* m_cpu_engine_radiobox;
+  wxRadioBox* m_cpu_engine_radiobox;
 };
